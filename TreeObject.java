822702171a4c5ca281b62a983c2171e819416b9c
hkhailee <42651565+hkhailee@users.noreply.github.com>
2018-11-14 19:36:02 -0700

    Update TreeObject.java

--- TreeObject.java
+++ TreeObject.java
@@ -1,5 +1,4 @@
-You may call the relevant class TreeObject
-to represent the objects. // this means?
+
 
 public class TreeObject {
 	
@@ -11,35 +10,4 @@ public class TreeObject {
 	
 		
 	}
-  
-  
-/*Returns the child TreeNode at index childIndex.*/
-
-  public TreeNode getChildAt(int childIndex){
-  }
- 
- /*Returns the number of children TreeNodes the receiver contains.*/
-  public int getChildCount(){
-  }
-  
- /*Returns the parent TreeNode of the receiver.*/
-  public  TreeNode getParent(){
-  }
-  
-  /*Returns the index of node in the receivers children. If the receiver does not contain node, -1 will be returned.*/
-  public int getIndex(TreeNode node){
-  }
-  
- /*Returns true if the receiver allows children.*/
-  public boolean getAllowsChildren(){
-  }
-  
- /*Returns true if the receiver is a leaf.*/
-  public boolean isLeaf(){
-  }
-  
- /*Returns the children of the receiver as an Enumeration.*/
-  public Enumeration children() {
-  }
-
 }
