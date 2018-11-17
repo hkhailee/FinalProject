039a2be183ee5a3495cc6e17435909d9964ce07b
hkhailee <42651565+hkhailee@users.noreply.github.com>
2018-11-16 19:37:06 -0700

    added in file scanner
    scanner only adds valid chars to the sequence and restarts the sequence once k has been reached

--- GeneBankCreateBTree.java
+++ GeneBankCreateBTree.java
@@ -1,15 +1,19 @@
+package finalProject;
+
 import java.io.File;
+import java.util.Scanner;
 
 public class GeneBankCreateBTree {
 
 	public static int cacheSize;
+	public static int k;
 
 	public static void main(String args[]) {
 		try {
 
-			//Checks to see if required size is present
+			// Checks to see if required size is present
 			if (args.length >= 4) {
-				
+
 				/*
 				 * if args 0 is 1 then use cache else no cache uses args 4 to get cache size
 				 */
@@ -20,7 +24,8 @@ public class GeneBankCreateBTree {
 						cacheSize = Integer.parseInt(args[4]); // cache size should never change
 					}
 				} else {
-					throw new Exception(); //invalid cache value given 
+					System.out.println(Integer.parseInt(args[0]));
+					throw new Exception(); // invalid cache value given
 				}
 
 				// if degree is 0 then find optimal degree and use that
@@ -38,10 +43,12 @@ public class GeneBankCreateBTree {
 				 * checks to see if k is within range
 				 */
 
-				if (Integer.parseInt(args[3]) < 1 || Integer.parseInt(args[3]) > 31) {
+				if (Integer.parseInt(args[3]) > 1 || Integer.parseInt(args[3]) < 31) {
 
-					int k = Integer.parseInt(args[3]); // Sequence length
+					k = Integer.parseInt(args[3]); // Sequence length
 				} else {
+
+					System.out.println(Integer.parseInt(args[3]));
 					throw new Exception(
 							"sequnce length k must be greater than or equal to 1 and less than or equal to 31");
 
@@ -50,26 +57,80 @@ public class GeneBankCreateBTree {
 				/*
 				 * debugging level
 				 */
-				if (Integer.parseInt(args[5]) == 0 || Integer.parseInt(args[5]) == 1) {
-					if (Integer.parseInt(args[5]) == 0) {
-						// Any diagnostic messages, help and status messages must be be printed on
-						// standard error stream
+
+				if (args.length > 4) {
+					if (Integer.parseInt(args[4]) == 0 || Integer.parseInt(args[4]) == 1) {
+						if (Integer.parseInt(args[4]) == 0) {
+							// Any diagnostic messages, help and status messages must be be printed on
+							// standard error stream
+						}
+						if (Integer.parseInt(args[4]) == 1) {
+							// The program writes a text file named dump, that has the following line
+							// format:
+							// DNA string: frequency. The dump file contains DNA string (corresponding to
+							// the key stored) and frequency in an inorder traversal. You can find a dump
+							// file
+						}
+						/*
+						 * debug level 0 or 1 (optional)
+						 */
+					} else {
+						throw new Exception(); // invalid debug value given
+
 					}
-					if (Integer.parseInt(args[5]) == 1) {
-						// The program writes a text file named dump, that has the following line
-						// format:
-						// DNA string: frequency. The dump file contains DNA string (corresponding to
-						// the key stored) and frequency in an inorder traversal. You can find a dump
-						// file
-					}
-					/*
-					 * debug level 0 or 1 (optional)
-					 */
-				} else {
-					throw new Exception(); // invalid debug value given
 				}
+
+				/*
+				 * scan a given kgb file and then send the corresponding sequence string to the
+				 * btree class
+				 */
+				boolean foundStart = false;
+				int bitNum = 0;
+				String sequence = "";
+				Scanner scan = new Scanner(new File(args[2]));
+
+				while (scan.hasNextLine()) {
+					Scanner s2 = new Scanner(scan.nextLine());
+
+					while (s2.hasNext()) {
+
+						if (foundStart == false) {
+							String s = s2.next();
+							if (s.equals("ORIGIN")) {
+								foundStart = true;
+							}
+						} else {
+
+							char seq = s2.next().charAt(0);
+							bitNum++;
+
+							if (seq == 'a' || seq == 't' || seq == 'c' || seq == 'g') { // only adds valid characters to sequence
+								
+
+								if (sequence.length() < k) { // while less than the given sequence size
+									sequence += seq;		//add it to the sequence
+									if(sequence.length() == k)
+									System.out.println(sequence);
+									
+								}
+								else {
+									sequence= "";	//if the sequence is greater than the limit restart
+									sequence += seq; //add the letter that has yet to be added
+								}
+
+							}
+
+						}
+					}
+
+				}
+				/*
+				 * next step is to assign each of these sequences with bit locations
+				 */
+				System.out.println(bitNum);
 			} else {
-				throw new Exception(); //required arguments do not exist
+				throw new Exception(); // required arguments do not exist
+
 			}
 		}
 
