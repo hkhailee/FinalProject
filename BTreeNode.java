e1c0d46da79e752970482ef0c44c3267317854be
mdhenke <38406783+mdhenke@users.noreply.github.com>
2018-12-06 17:56:31 -0700

    Update BTreeNode.java

--- BTreeNode.java
+++ BTreeNode.java
@@ -122,7 +122,7 @@ public class BTreeNode {
 	
 	public void diskWrite() throws Exception {
 		
-	    raf.seek(byteOffset); // Go to byte at offset position 5.
+	    raf.seek(byteOffset); // seek to byte offset
 	    raf.writeInt(byteOffset);
 	    raf.writeInt(getNumObjects());
 	    if (isLeaf) {
@@ -149,8 +149,39 @@ public class BTreeNode {
 	    for (j = i; j <= maxObjects; j++) {
 	    	raf.writeLong(-1);
 	    	raf.writeInt(0);
+	    }	    
+	}
+	
+	public void diskWriteAsRoot() throws Exception {
+		
+	    raf.seek(4); // Go to byte at offset position 4.
+	    raf.writeInt(byteOffset);
+	    raf.writeInt(getNumObjects());
+	    if (isLeaf) {
+	    	raf.writeInt(1);
 	    }
-	    
+	    else raf.writeInt(0);
+	    raf.writeInt(parentPtr);
+	    int i, j;
+	    //Write all pointers. Unused pointers will be written as 0.
+	    for (i = 1; i < childPtrs.size(); i++) {
+	    	if (i <= maxObjects+1) {
+	    		raf.writeInt(childPtrs.get(i));
+	    	}
+	    }
+	    for (j = i; j <= maxPtrs; j++) {
+	    	raf.writeInt(-1);
+	    }
+	    for (i = 1; i <= numObjects; i++) {
+	    	if (i <= maxObjects) {
+	    		raf.writeLong(objects.get(i).getStream());
+		    	raf.writeInt(objects.get(i).getFrequency());
+	    	}
+	    }
+	    for (j = i; j <= maxObjects; j++) {
+	    	raf.writeLong(-1);
+	    	raf.writeInt(0);
+	    }	    
 	}
 	
 	/**
@@ -189,7 +220,41 @@ public class BTreeNode {
 	    	}
 	    }
 	    newNode.setObjects(treeObjs);
-	    List<BTreeNode> allChildren = new ArrayList<BTreeNode>();
+	    return newNode;
+	}
+	
+	public BTreeNode diskReadRoot() throws IOException {
+		BTreeNode newNode = new BTreeNode(4, maxObjects, raf);
+	    raf.seek(4);
+	    raf.readInt();
+	    newNode.setNumObjects(raf.readInt());
+	    newNode.setIsLeaf(raf.readInt());
+	    raf.readInt();
+	    newNode.setParentPointer(byteOffset);
+	    
+	    //set child pointers
+	    List<Integer> ptrs = new ArrayList<Integer>();
+	    ptrs.add(-1);
+	    for (int i = 0; i < maxPtrs; i++) {
+	    	int x = raf.readInt();
+	    	if (x != -1) {
+	    		ptrs.add(x);
+	    	}
+	    }
+	    newNode.setChildPointers(ptrs);
+	    
+	    //set tree objects
+	    
+	    List<TreeObject> treeObjs = new ArrayList<TreeObject>();
+	    treeObjs.add(null);
+	    for (int i = 0; i < maxObjects; i++) {
+	    	long x = raf.readLong();
+	    	int y = raf.readInt();
+	    	if (x != -1) {
+	    		treeObjs.add(new TreeObject(x, y));
+	    	}
+	    }
+	    newNode.setObjects(treeObjs);
 	    return newNode;
 	}
 }
